663558fb3887104d6539c41b4ca17645541c708a
Daviichii89 <davichi89@gmail.com>
2023-08-31 00:42:31 +0200

    Create routes, pages components  and a navbar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
-import MangaList from './components/MangaList'
-import SearchComponent from './components/SearchComponent'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
 
 function App() {
   return (
-    <main className='min-h-screen text-center p-4'>
-      <h1 className='text-3xl font-bold underline mb-6'>
-        Manga
-      </h1>
-      <SearchComponent />
-      <MangaList />
+    <main className="min-h-screen text-center p-4">
+      <h1 className="text-3xl font-bold underline mb-6">Manga</h1>
+      <BrowserRouter>
+        <nav className="flex justify-evenly">
+          <Link
+            to="/"
+            className="border w-full bg-slate-300 hover:bg-slate-100"
+          >
+            Inicio
+          </Link>
+          <Link
+            to="/collection"
+            className="border w-full bg-slate-300 hover:bg-slate-100"
+          >
+            Mi collección
+          </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+        </Routes>
+      </BrowserRouter>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import SearchComponent from "../components/SearchComponent";
+import MangaList from "../components/MangaList";
+
+const Home = () => {
+  return (
+    <>
+      <SearchComponent />
+      <MangaList />
+    </>
+  );
+};
+
+export default Home;
