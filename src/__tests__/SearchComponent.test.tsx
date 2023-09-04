61152c016171a6afa5d41915156848323d3bfbcd
Daviichii89 <davichi89@gmail.com>
2023-09-04 11:18:57 +0200

    Testing the SearchComponent

--- src/__tests__/SearchComponent.test.tsx
+++ src/__tests__/SearchComponent.test.tsx
@@ -1,18 +1,46 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-
-import SearchComponent from '../components/SearchComponent'
 import { Provider } from 'react-redux'
+import * as reactRedux from 'react-redux';
 import { store } from '../redux/store'
 
+import SearchComponent from '../components/SearchComponent'
+
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useDispatch: jest.fn(),
+    useSelector: jest.fn()
+}));
+
 describe('<SearchComponent />', () => {
-    it('Should display input element', () => {
-        const user = userEvent
+    const useDispatchMock = reactRedux.useDispatch
+    const useSelectorMock = reactRedux.useSelector
+    beforeEach(() => {
+        useDispatchMock.mockImplementation(() => () => {})
+        useSelectorMock.mockImplementation(selector => selector(mockStore))
+    })
+    afterEach(() => {
+        useDispatchMock.mockClear()
+        useSelectorMock.mockClear()
+    })
+    it('Should display input element', async () => {
         render(<Provider store={store}>
                 <SearchComponent />
             </Provider>)
         expect(screen.getByPlaceholderText(/search.../i)).toBeInTheDocument()
-
-        user.click(screen.getByRole('button', {name: /buscar/i}))
+        await userEvent.click(screen.getByRole('button', {name: /buscar/i}))
+    })
+    it('Should updates keyword state on input change or the button is clicked', async () => {
+        render(<Provider store={store}>
+            <SearchComponent />
+        </Provider>)
+        const handleSubmit = jest.fn()
+        const input = screen.getByPlaceholderText(/search.../i)
+        await userEvent.type(input, 'Naruto' )
+        expect(input).toHaveValue('Naruto')
+        const button = screen.getByText(/buscar/i)
+        await userEvent.click(button)
+        expect(handleSubmit).toHaveBeenCalled()
+        expect(input).toHaveValue('')
     })
 })
