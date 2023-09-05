ae3c953906b2034739e571b00e5c7356e2c5b68f
Daviichii89 <davichi89@gmail.com>
2023-09-05 22:01:28 +0200

    Test SearchComponent.tsx

--- src/__tests__/SearchComponent.test.tsx
+++ src/__tests__/SearchComponent.test.tsx
@@ -1,48 +1,31 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
-import { Provider } from 'react-redux'
-import * as reactRedux from 'react-redux';
-import { store } from '../redux/store'
+import SearchComponent from '../components/SearchComponent';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 
-import SearchComponent from '../components/SearchComponent'
+describe('SearchComponent', () => {
+  it('Should all the elements in the component', () => {
+    render(
+        <Provider store={store}>
+            <SearchComponent />;
+        </Provider>
+    )
+    
+    expect(screen.getByPlaceholderText('Search for your mangas...')).toBeInTheDocument();
+    expect(screen.getByText('Search')).toBeInTheDocument();
+  });
 
-jest.mock('react-redux', () => ({
-    ...jest.requireActual('react-redux'),
-    useDispatch: jest.fn(),
-    useSelector: jest.fn()
-}));
-
-describe('<SearchComponent />', () => {
-    const useDispatchMock = reactRedux.useDispatch
-    const useSelectorMock = reactRedux.useSelector
-    beforeEach(() => {
-        useDispatchMock.mockImplementation(() => () => {})
-        useSelectorMock.mockImplementation(selector => selector(mockStore))
-    })
-    afterEach(() => {
-        useDispatchMock.mockClear()
-        useSelectorMock.mockClear()
-    })
-    it('Should display input element', async () => {
-        render(<Provider store={store}>
-                <SearchComponent />
-            </Provider>)
-        expect(screen.getByPlaceholderText(/search for your mangas.../i)).toBeInTheDocument()
-        await userEvent.click(screen.getByRole('button', {name: /search/i}))
-    })
-    it('Should updates keyword state on input change or the button is clicked', async () => {
-        render(<Provider store={store}>
+  it('Should on click button clear the input value', () => {
+    render(
+        <Provider store={store}>
             <SearchComponent />
-        </Provider>)
-        const handleSubmit = jest.fn()
-        const input = screen.getByPlaceholderText(/search for your mangas.../i)
-        await userEvent.type(input, 'Naruto' )
-        expect(input).toHaveValue('Naruto')
-        const button = screen.getByText(/search/i)
-        await userEvent.click(button)
-        expect(handleSubmit).toHaveBeenCalled()
-        expect(input).toHaveValue('')
-    })
-})
+        </Provider>
+    );
+    const inputElement = screen.getByPlaceholderText('Search for your mangas...') as HTMLInputElement;
+    userEvent.type(inputElement, 'One Piece');
+    userEvent.click(screen.getByText('Search'))
+
+    expect(inputElement.value).not.toBe('One Piece');
+  });
+});
--- src/__tests__/getManga.test.ts
+++ src/__tests__/getManga.test.ts
@@ -1,5 +1,11 @@
+import getManga from '../api/getManga';
+
 describe('getManga', () => {
-    it('render getManga', () => {
-        
-    })
-})
+  it('debe devolver mangas correctamente', async () => {
+    const mangaTitle = 'One Piece';
+    const result = await getManga(mangaTitle);
+
+    expect(result).toBeDefined();
+    expect(result[0].title).toBe(mangaTitle);
+  });
+});
